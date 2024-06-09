c634a2b338340d4a55866f0864682a588dad2527
tmntee <107428229+tmntee@users.noreply.github.com>
2024-06-08 20:47:25 -0400

    Refactor code into different objects, make it possible for player to place their move on the board

--- script.js
+++ script.js
@@ -1,204 +1,67 @@
-let Checkboard = (function(){
-    let box = (number, occupied, playerOccupying) => { return { number, occupied, playerOccupying} };
-    const boardBoxes = [box(1, false, ''), box(2, false, ''), box(3, false, ''), 
-                        box(4, false, ''), box(5, false, ''), box(6, false, ''),
-                        box(7, false, ''), box(8, false, ''), box(9, false, '')];
 
-    let place = (boxNumber, playerName) => {
-        let affectedBox = boardBoxes.find((b) => b.number === boxNumber);
-        if (affectedBox !== undefined && affectedBox !== NaN) {
-            if (affectedBox.occupied === false) {
-                affectedBox.occupied = true;
-                affectedBox.playerOccupying = playerName;
-                displayBoard();
-                return true;
-            } else {
-                console.log("Spot is already occupied.");
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
+let Gameboard = (function(){
 
-    //let displayBoard = function() {
-        //console.log(`${boardBoxes.at(0).XorO}  |  ${boardBoxes.at(1).XorO}  |  ${boardBoxes.at(2).XorO}\n${boardBoxes.at(3).XorO}  |  ${boardBoxes.at(4).XorO}  |  ${boardBoxes.at(5).XorO}\n${boardBoxes.at(6).XorO}  |  ${boardBoxes.at(7).XorO}  |  ${boardBoxes.at(8).XorO}`)
-    //}
 
-    let occupationStatus = () => {
-        boxStatuses = [];
-        boardBoxes.forEach((b) => {
-            let tempBox = (boxNumber, playerOccupying) => {
-                return {boxNumber, playerOccupying}
-            }
-
-            boxStatuses.push(tempBox(b.number, b.XorO))
-        })
-
-        return boxStatuses;
-    }
-
-    let restartBoard = () => {
-        boardBoxes.forEach((box) => {
-            box.occupied = false;
-            box.playerOccupying = ' ';
-        })
-    }
-
-    return {place, occupationStatus, restartBoard};
-})();
+})
 
 let Player = function(name, botOrNot, color){
     const playerName = name;
     let bot = botOrNot;
+    let sequence = '';
 
     let move = () => {
-        let boxNumber = ''; 
-        if (bot === false) {
-            while (Checkboard.place(selectedBox, playerName) === false && /[1-9]/.test(boxNumber) === false) {
-                var selectedBox = Number(prompt("Select a box to place your move in. 1-9"));
-            }
-            boxNumber = selectedBox; 
-
-        } else {
-            while (Checkboard.place(randomIndex, playerName) === false) {
-                var randomIndex = Math.floor(Math.random() * 9);
-            }
-            boxNumber = randomIndex;
-        }
     }
 
-    return {move, playerName, color};
+    return {move, playerName, color, sequence};
 };
 
 let GameManager = (function(){
     let winningSequences = ["123", "159", "147", "258", 
                             "357", "369", "456", "789"]
-
-    let player1, player2;
-    let firstPlayer, secondPlayer;
-
     let winner;
 
-    let checkIfWin = () => {
-        let winOrNot = false;
-        let player1Sequence = "";
-        let player2Sequence = "";
-        let statuses = Checkboard.occupationStatus();
-        statuses.forEach((status) => {
-            if (status.playerOccupying === player1.playerMoveType) {
-                player1Sequence += `${status.boxNumber}`;
-            } else if (status.playerOccupying === player2.playerMoveType) {
-                player2Sequence += `${status.boxNumber}`;
-            } 
+    let startGame = (firstPlayer, secondPlayer) => {
+        let player1 = firstPlayer;
+        let player2 = secondPlayer;
+        let currentPlayerMoving;
+        let gameStatusText = document.querySelector("div.game-status");
+        gameStatusText.textContent = "Determining who goes first..."
+
+        let diceRoll = Math.floor(Math.random * 10);
+        if (diceRoll < 5) {
+            currentPlayerMoving = player1;
+            gameStatusText.textContent = `${player1.playerName} goes first.`
+        } else {
+            currentPlayerMoving = player2;
+            gameStatusText.textContent = `${player2.playerName} goes first.`
+        }
+        console.log(currentPlayerMoving);
+
+        let buttons = Array.from(document.querySelectorAll("button.board-button"));
+        buttons.forEach((button) => {
+            button.addEventListener('click', () => {
+
+                let buttonImg = button.querySelector("img");
+                if (currentPlayerMoving.playerName === player1.playerName) {
+
+                    buttonImg.setAttribute("src", "images/x.svg");
+
+                } else if (currentPlayerMoving.playerName === player2.playerName) {
+
+                    buttonImg.setAttribute("src", "images/circle.svg");
+                }
+
+                button.appendChild(buttonImg);
+                button.style.backgroundColor = currentPlayerMoving.color;
+                currentPlayerMoving.sequence += button.value;
+                console.log(currentPlayerMoving.sequence);
+            })
         })
-
-        console.log(player1Sequence);
-        console.log(player2Sequence);
-
-        for (let i = 0; i < winningSequences.length; i++) {
-            if (player1Sequence === winningSequences.at(i)) {
-                winner = "Player 1 Wins!";
-                winOrNot = true;
-                break;
-            } else if (player2Sequence === winningSequences.at(i)) {
-                winner = "Player 2 Wins!";
-                winOrNot = true;
-                break;
-            }
-        }
-
-        return winOrNot;
     }
+        
+    
 
-    let rollChance = () => {
-        return Math.floor(Math.random() * 10);
-    }
-
-    let gameSetup = () => {
-        console.log("Welcome to Tic Tac Toe!");
-
-        let botAnswer = '';
-        while ((botAnswer !== "Y") && (botAnswer != "N")) {
-            botAnswer = prompt("Play against a bot? Y/N").toLocaleUpperCase();
-            console.log(botAnswer);
-        };
-
-        let moveTypeAnswer = '';
-        while ((moveTypeAnswer !== "X") && (moveTypeAnswer !== "O")) {
-            moveTypeAnswer = prompt("Do you want to be X or O?").toLocaleUpperCase();
-            console.log(moveTypeAnswer);
-        }
-
-        let player2IsBot;
-        let oppositeMoveType;
-
-        if (botAnswer === "Y") {
-            player2IsBot = true;
-        } else {
-            player2IsBot = false
-        };
-
-        if (moveTypeAnswer === "X") {
-            oppositeMoveType = "O";
-        } else {
-            oppositeMoveType = "X";
-        }
-
-        player1 = Player(moveTypeAnswer, false);
-        player2 = Player(oppositeMoveType, player2IsBot);
-        startGame();
-    }
-
-    let determinePlayerTurn = (moveNumber) => {
-        let turnDeterminer = (moveNumber % 2) 
-        switch (turnDeterminer) {
-            case 0:
-                playerMove(firstPlayer);
-                break;
-
-            case 1:
-                playerMove(secondPlayer);
-                break;
-        }
-    }
-
-    let startGame = () => {
-        const movesMax = 9;
-        let movesTracker = 0;
-        let dice = rollChance();
-
-        if (dice < 5) {
-            firstPlayer = player2;
-            secondPlayer = player1;
-        } else {
-            firstPlayer = player1;
-            secondPlayer = player2;
-        }
-        console.log(`Player ${firstPlayer.playerMoveType} goes first.`)
-
-        for (let i = 0; i < movesMax; i++) {
-            if (checkIfWin() === true) {
-                break;
-            }
-            determinePlayerTurn(i);
-            ++movesTracker;
-        }
-
-
-        if (movesTracker === movesMax) {
-            winner = "It's a tie."
-        }
-
-        console.log(`${winner}`);
-    }
-
-    let playerMove = (p) => {
-        p.move();
-        console.log(`Player ${p.playerMoveType} has gone.`)
-    }
-
-    return {gameSetup};
+    return {startGame}
 })();
 
 // player customization scripts 
@@ -320,6 +183,7 @@ let DOMManager = (() => {
         goBackButton.addEventListener('click', () => {
             loadInitialPage();
         })
+        GameManager.startGame(PlayerInformationManager.getPlayer1(), PlayerInformationManager.getPlayer2());
     }
     
     return {loadInitialPage, loadGamePage}
